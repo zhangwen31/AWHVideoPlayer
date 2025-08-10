c216c8dec97d5c7daa6199e958584778243ef216
王恒 <1066026709@qq.com>
2025-08-10 08:35:14 +0800

    修改配置

---
+++ AWHVideoPlayer.framework/Headers/AWHVPAlarmSubscriptionTopLineTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  AWHVPAlarmSubscriptionTopLineTableViewCell.h
+//  AWHVideoPlayer
+//
+//  Created by 王恒 on 2023/5/20.
+//
+
+#import <UIKit/UIKit.h>
+#import <AWHVideoPlayer/AWHVPAlarmSubscriptionTableViewCell.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AWHVPAlarmSubscriptionTopLineTableViewCell : AWHVPAlarmSubscriptionTableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
